eafd93d5c540160e984aee1e833975d3882d5ff1
dipakTR <110381280+dipakTR@users.noreply.github.com>
2026-03-04 12:37:53 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import router from "./routes/router";
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.use("/", router);
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 app.listen(PORT, () => console.log(`Server Started on PORT ${PORT} 🎉`));
